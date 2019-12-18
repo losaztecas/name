03f84441d38415be334e705117c9ef33b21a6cff
losaztecas <57035797+losaztecas@users.noreply.github.com>
2019-12-19 00:35:00 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Project
+                    project spreman
                 </div>
                 <div>
                     Laravel - production

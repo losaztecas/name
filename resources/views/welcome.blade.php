dbbcd0abe4395e46d6ca63760a301f213c55e225
losaztecas <57035797+losaztecas@users.noreply.github.com>
2019-12-19 00:49:55 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel - dev
+                    i ovaj project takodzie spreman
                 </div>
 
                 <div class="links">
